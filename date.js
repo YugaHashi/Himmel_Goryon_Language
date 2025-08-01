972ec070980a994c8ec574828c79968f03f517e4
YugaHashi <hashiguchiyuga@gmail.com>
2025-08-01 12:42:43 +0900

    Delete date.js

--- date.js
+++
@@ -1,8 +0,0 @@
-// URL に ?date=YYYY-MM-DD を自動付与（最初の1回のみ）
-(function(){
-  const today = new Date().toISOString().slice(0,10);
-  if (!/(\?|&)date=/.test(location.search)) {
-    const sep = location.search ? '&' : '?';
-    location.replace(location.pathname + location.search + sep + 'date=' + today);
-  }
-})();
